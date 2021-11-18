595805d8739f4157c25067b82a15ca594f66d209
andre-mare <andre.mare@gmail.com>
2021-11-18 14:50:44 +0100

    Rename Interaction Name

--- interaction-examples/interaction-example1/src/main/java/com/backbase/flow/example/interaction/handler/PersonalMessageGenerationHandler.java
+++ interaction-examples/interaction-example1/src/main/java/com/backbase/flow/example/interaction/handler/PersonalMessageGenerationHandler.java
@@ -1,12 +1,12 @@
 package com.backbase.flow.example.interaction.handler;
 
 
+import com.backbase.flow.example.casedata.PersonalMessageState;
 import com.backbase.flow.example.interaction.dto.RequestDTO;
 import com.backbase.flow.example.interaction.dto.ResponseDTO;
 import com.backbase.flow.interaction.engine.action.ActionHandler;
 import com.backbase.flow.interaction.engine.action.ActionResult;
 import com.backbase.flow.interaction.engine.action.InteractionContext;
-import com.backbase.flow.example.casedata.CaseDefinitionExample1;
 
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +14,9 @@ import org.springframework.stereotype.Component;
 
 
 @Slf4j
-@Component("step-one-handler")
+@Component("generate-personal-message")
 @RequiredArgsConstructor
-public class StepOneHandler implements ActionHandler<RequestDTO, ResponseDTO> {
+public class PersonalMessageGenerationHandler implements ActionHandler<RequestDTO, ResponseDTO> {
 
 
     @Override
@@ -26,11 +26,11 @@ public class StepOneHandler implements ActionHandler<RequestDTO, ResponseDTO> {
 
         log.info(personalMessage);
 
-        CaseDefinitionExample1 simpleCaseDefinition = new CaseDefinitionExample1();
-        simpleCaseDefinition.setFirstName(requestDTO.getFirstName());
-        simpleCaseDefinition.setLastName(requestDTO.getLastName());
+        PersonalMessageState messageState = new PersonalMessageState();
+        messageState.setFirstName(requestDTO.getFirstName());
+        messageState.setLastName(requestDTO.getLastName());
 
-        interactionContext.saveCaseDataToReadCaseData(simpleCaseDefinition);
+        interactionContext.saveCaseDataToReadCaseData(messageState);
 
         ResponseDTO response = new ResponseDTO();
         response.setMessage(personalMessage);
